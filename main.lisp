97498bca50c3aa9d8d3be6ba71ea0972a3710d70
drasken <contumax90@yahoo.it>
2026-02-28 17:08:06 +0100

    Added run-interpreter function to use in my main loop

--- main.lisp
+++ main.lisp
@@ -8,6 +8,15 @@
 ;; File to read, use as test. Hardcoded for the moment.
 (defparameter input-file  "./repeat.bf")
 
+(defun run-interpreter (source)
+  "TODO: add here docstring"
+  (let ((state (make-bf-state :array (make-array 30000 :initial-element 0)
+                              :p-counter 0
+                              :a-counter 0
+                              :jump-table (pre-parse source))))
+    (loop while (< (bf-state-p-counter state) (length source)) do
+      (execute (char source (bf-state-p-counter state)) state)
+      (incf (bf-state-p-counter state)))))
 
 (defun main ()  ;; TODO: here use my main loop
   "My main function in which I encapsulate my interpreted main loop"
